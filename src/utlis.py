0e240ec38c782d909a06f0e561287fc81830ee4a
Bishal Roy <roybishal362@gmail.com>
2024-09-07 16:47:40 +0530

    App.py

--- src/utlis.py
+++ src/utlis.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+import pickle
 
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
@@ -54,4 +55,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
     except Exception as e:
          raise CustomException(e, SystemError)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
         
